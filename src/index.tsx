c46be3fca15bb8f8ff32d60152191de213edb70c
Unri <pokevin17@gmail.com>
2023-11-23 00:47:27 +0100

    feat: Add Biome

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 
-import "./styles.css";
 import App from "./App";
+import "./styles.css";
 
 render(() => <App />, document.getElementById("root") as HTMLElement);
