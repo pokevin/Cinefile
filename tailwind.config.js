69de499d7a68bb49fc60944d055f87a50dc49a25
Unri <pokevin17@gmail.com>
2023-12-23 19:02:41 +0100

    feat: Add custom tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        background: "rgb(var(--color-bg-primary) / <alpha-value>)",
+        body: "rgb(var(--color-text) / <alpha-value>)",
+        "body-secondary": "rgb(var(--color-text) / 0.6)",
+      },
+    },
   },
   plugins: [],
 };
