fdf3634f2704b03370f439cbdedc9a830952ee6d
Unri <pokevin17@gmail.com>
2024-02-08 22:59:52 +0100

    fix(PosterImage): Handle wrong src url

--- src/components/PosterImage.tsx
+++ src/components/PosterImage.tsx
@@ -1,16 +1,24 @@
 import clsx from "clsx";
+import { createSignal } from "solid-js";
 import type { JSX } from "solid-js/jsx-runtime";
 
 export const PosterImage = ({
   alt,
   ...props
-}: JSX.ImgHTMLAttributes<HTMLImageElement>) => (
-  <img
-    {...props}
-    alt={alt}
-    class={clsx(
-      "aspect-poster w-40 object-cover shadow-poster bg-black/30",
-      props.class,
-    )}
-  />
-);
+}: JSX.ImgHTMLAttributes<HTMLImageElement>) => {
+  const [src, setSrc] = createSignal(props.src);
+  return (
+    <img
+      {...props}
+      src={src()}
+      onerror={() => setSrc("")}
+      alt={alt}
+      width={160}
+      height={237}
+      class={clsx(
+        "aspect-poster w-40 object-cover text-white shadow-poster bg-black/30",
+        props.class,
+      )}
+    />
+  );
+};
